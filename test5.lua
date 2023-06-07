079f02eb84341ba8a8d1518abc325fde7afdc80b
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2023-06-07 13:29:12 -0400

    Update test5.lua

--- test5.lua
+++ test5.lua
@@ -2787,7 +2787,7 @@ local function AutoPickup()
 
     for Index, Value in next, workspace.placeFolders.items:GetChildren() do 
         local itemName = Value.Name
-        if itemName == "coin piece" or itemName == "crystal beetle" or itemName == "megaphone" or itemName == "giant token" or itemName == "dustworm longbow" or itemName == "sand ravaged bow" or itemName == "dustworm cudgel" or itemName == "sand ravaged scimitar" or itemName == "chitin scythe" or itemName == "stingtail staff" or itemName == "azariah edge" or itemName == "scarab tongue boots" or itemName == "auktufiti ballista" then
+        if itemName == "coin piece" or itemName == "crystal beetle" or itemName == "megaphone" or itemName == "ancient weapon attack scroll dex" or itemName == "giant token" or itemName == "dustworm longbow" or itemName == "sand ravaged bow" or itemName == "dustworm cudgel" or itemName == "sand ravaged scimitar" or itemName == "chitin scythe" or itemName == "stingtail staff" or itemName == "azariah edge" or itemName == "scarab tongue boots" or itemName == "auktufiti ballista" then
             if (Value.Position - Hitbox.Position).Magnitude < 100000000 then 
                 PickupItem(Value)
                 if itemName == "dustworm longbow" or itemName == "sand ravaged bow" or itemName == "dustworm cudgel" or itemName == "sand ravaged scimitar" or itemName == "chitin scythe" or itemName == "stingtail staff" or itemName == "azariah edge" or itemName == "scarab tongue boots" or itemName == "auktufiti ballista" then
